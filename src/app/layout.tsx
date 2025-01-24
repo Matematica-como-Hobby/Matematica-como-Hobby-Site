5d86644c4440ce2643be517fa70407fddbfeeff5
DaveAlSv <davealvessilvino@gmail.com>
2025-01-24 15:15:38 -0300

    new visual style- for desktop, tailwind 4.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
+import logo from '@/public/logo.png'
+import Image from 'next/image';
+
 import '@/styles/global.css';
 
 export default function RootLayout({ children, } : { children : React.ReactNode }) {
   return (
     <html lang="en">
       <body className='bg-primary'>
-        <header className='absolute top-0 w-full flex justify-end bg-transparent z-[1] text-white py-5 px-20 border-b border-white '>
-          <nav>
-            <div className='flex gap-7 ' style={{fontFamily: "Poiret One"}}>
+        <header className='w-full bg-green-mth flex justify-center text-white py-2 px-20 '>
+          <Image className="w-14" src={logo} alt="mth-logo"/>
+
+          <nav className='absolute right-10 py-4'>
+            <div className='flex self-end gap-7 ' style={{fontFamily: "Poiret One"}}>
               <a>theme</a>
               <a>docs</a>
               <a>about</a>
             </div>
           </nav>
+          
         </header>
 
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,33 @@
 import Image from 'next/image';
-import background from '@/public/background.jpg'
 import card_oint from '@/public/card-oint.png'
 import "@/styles/neon.css"
 
 export default function Page() {
   return <main>
-    <Image alt="freepick-2" src={background} quality={100} className='absolute top-0 z-[0] pointer-events-none w-svw h-svh opacity-50'/> 
 
-    <div className='relative z-[1] text-white w-svw h-svh grid grid-cols-2'>
-      <div className='flex flex-col ml-48 w-full pr-11 justify-center'>
-        <h1 className="text-2xl p-8 font-[Almendra]">Livros , Resolução & Comunidade - Matematica como Hobby</h1>
-        
-        <p className="text-2xl leading-10 my-3" style={{fontFamily : "Carattere"}}>A matemática, frequentemente percebida como uma disciplina desafiadora, pode se transformar em um hobby fascinante quando explorada por meio de  livros, resolução de problemas e a força da comunidade. Esses elementos inspiram os leitores a apreciar a beleza dos números e a lógica por trás das fórmulas. Resolver desafios matemáticos oferece um prazer único, estimulando tanto o raciocínio crítico quanto a criatividade. Assim, a matemática se revela não apenas uma ciência, mas uma emocionante jornada de descobertas e conexões.</p>
+    <div className='relative z-1 flex flex-col justify-center mt-32 m-auto text-white text-center'>
+      
+      <h1 className="text-5xl font-[Almendra]">Livros , Resolução & Comunidade - Matematica como Hobby</h1>
+      
+      <p className="text-2xl leading-10 mt-32 mx-auto w-3/5" style={{fontFamily : "Carattere"}}>A matemática, frequentemente percebida como uma disciplina desafiadora, pode se transformar em um hobby fascinante quando explorada por meio de  livros, resolução de problemas e a força da comunidade. Esses elementos inspiram os leitores a apreciar a beleza dos números e a lógica por trás das fórmulas. Resolver desafios matemáticos oferece um prazer único, estimulando tanto o raciocínio crítico quanto a criatividade. Assim, a matemática se revela não apenas uma ciência, mas uma emocionante jornada de descobertas e conexões.</p>
 
-        <div className='flex flex-row gap-14 ml-20 mt-10 h-12'>
-          <button className='neonButton w-32 rounded-md text-sm'>
-            ver livros
-          </button>
+      <div className='flex gap-14 m-auto mt-32 h-12'>
+        <button className='neonButton w-32 rounded-md text-sm'>
+          ver livros
+        </button>
 
-          <button className='bg-white bg-opacity-10 w-32 rounded-md text-sm'>
-            docs
-          </button>
+        <button className='bg-white/10 w-32 rounded-md text-sm'>
+          docs
+        </button>
 
-        </div>
-
-      </div>
-
-      <div className='py-28 pointer-events-none'>
-        <Image alt="card" src={card_oint} className=''/>
       </div>
 
     </div>
+
+    <div className='relative'>
+      <Image alt="card" src={card_oint} className='absolute z-0 -top-40 left-10 opacity-60 w-[30%] pointer-events-none'/>
+    </div>
+
+
   </main>
 }
