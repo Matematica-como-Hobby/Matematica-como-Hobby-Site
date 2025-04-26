0f0f13af7fb3c2cffd7f0f597e3bdcbdc1d8910b
Sr. Coffe <davealvessilvino@gmail.com>
2025-04-26 20:20:59 -0300

    create an oAuth page to receive the code

---
+++ src/app/oAuth/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
+
+
+export default function Page(){
+
+  const code = useSearchParams().get("code") // get the parameter code from url like (https://example.com/oAuth?code=aaaaaa)
+  const router = useRouter() // router for redirect
+
+  useEffect(() => {
+    if (code){
+      // get and put the token of github, in a cookie
+      fetch("api/oAuth", {
+        method : "POST",
+        headers : {
+          'Content-Type': 'application/json',
+        },
+        body : JSON.stringify({
+          code : code
+        })
+      }).then(response => {
+        router.push(response.url) // redirect to /books if sucessfuly or to home if filed 
+      })
+
+    }
+  })
+  
+  return <></>
+}
